eb63b2946091b03ff5527ee0eb93b566991686be
9ravity00 <imprajwal793@gmail.com>
2025-12-22 14:07:37 +0530

    Include wasm build output for Vercel

---
+++ public/wasm/askii_wasm_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_processedframe_free: (a: number, b: number) => void;
+export const processedframe_chars: (a: number) => [number, number];
+export const processedframe_colors: (a: number) => [number, number];
+export const process_frame_with_color: (a: number, b: number, c: number, d: number, e: number, f: number) => number;
+export const apply_edge_detection: (a: number, b: number, c: number, d: number, e: number) => [number, number];
+export const apply_contrast: (a: number, b: number, c: number, d: number, e: number) => [number, number];
+export const process_frame: (a: number, b: number, c: number, d: number, e: number) => [number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_start: () => void;
